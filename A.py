583cb7573b9acbe7b51dc95979416aa23824e434
alejandru00 <alejandrolliber2004@gmail.com>
2023-09-21 19:57:04 +0200

    commit

--- A.py
+++ A.py
@@ -3,7 +3,7 @@ from math import ceil
 t = int(input())
 
 for _ in range(t):
-    a, b, c = map(int,input().split)        # map hace que se aplique int a cada esto de la cadena
+    a, b, c = map(int,input().split())        # map hace que se aplique int a cada esto de la cadena
 
     d = abs(a-b)                            # abs para valor absoluto
     print(ceil(d / (2*c)))
