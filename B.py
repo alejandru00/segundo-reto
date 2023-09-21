bd71d092530a69a7d0f7dd140a2795969d09f3f5
alejandru00 <alejandrolliber2004@gmail.com>
2023-09-21 19:50:26 +0200

    commit

--- B.py
+++ B.py
@@ -1,1 +1,16 @@
 
+# Leer la cantidad de elementos en la lista
+N = int(input())
+
+# Leer la lista de enteros
+A = list(map(int, input().split()))
+
+# Inicializar la suma de los valores absolutos
+sum_abs = 0
+
+# Calcular la suma de los valores absolutos de los elementos
+for num in A:
+    sum_abs += num
+
+# Imprimir la suma como resultado
+print(sum_abs)
